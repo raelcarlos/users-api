4e25daa606a7cae7cecf92b5ca072263bb9c9e1b
Israel de S. Carlos <rael.comp@gmail.com>
2018-07-26 20:37:07 -0300

    added endpoint to add new user from json file

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,39 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 
+var user = {
+    "user4" : {
+        "name" : "mohit",
+        "password" : "password4",
+        "profession" : "teacher",
+        "id": 4
+    }
+}
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  fs.readFile('./users.json', 'utf8', function (err, data) {
+    fs.readFile('./users.json', 'utf8', function (err, data) {
         console.log(data);
         res.end(data);
+    });
+});
+
+router.post('/addold', function (req, res) {
+    fs.readFile( './users.json', 'utf8', function (err, data) {
+        data = JSON.parse( data );
+        data["user4"] = user["user4"];
+        console.log( data );
+        res.end( JSON.stringify(data));
+    });
+});
+
+router.post('/add', function (req, res) {
+    fs.readFile( './users.json', 'utf8', function (err, data) {
+        data = JSON.parse( data );
+        var new_user = req.body;
+        data["user4"] = new_user;
+        console.log( data );
+        res.end( JSON.stringify(data));
     });
 });
 
